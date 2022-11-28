9c77251722acad161573fb0f7da60dcb5f9efc89
marwan Emad <marwan@sbs-sa.com>
2022-11-28 13:02:24 +0300

    print_journal_entry
    print_journal_entry

---
+++ print_journal_entry/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'SBS Print Journal Entries',
+    'version': '15.0.0.0',
+    'author': "SBS",
+    'website': "https://sbs-sa.com/",
+    'depends': ['base','account','web'],
+    'data': [
+        'report/views.xml',
+        'report/report_journal_entries.xml',
+        'report/report_journal_entries_view.xml',
+        'report/payment_print.xml',
+    ],
+}
